070222708bef56741770ab9561cd77dfee7f57cd
张小奕 <zxyqwe_2004@126.com>
2017-05-16 12:26:02 +0800

    qrcode

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,11 +1,25 @@
 <?php
+
 namespace app\index\controller;
+
+use Endroid\QrCode\QrCode;
 
 class Index
 {
-    public function index($did=3)
+    public function index()
     {
-        cache('123','123');
-        return [(int)input('get.id')+$did];
+        $qrCode = new QrCode('Life is too short to be generating QR codes');
+        $qrCode
+            ->setSize(300)
+            ->setErrorCorrection(QrCode::LEVEL_HIGH)
+            ->setLogo($qrCode->getImagePath() . DS . 'logo.png')
+            ->setLogoSize(150)
+            ->setLabelFontPath(APP_PATH . "../public/static/noto_sans.otf")
+            ->setLabelFontSize(25)
+            ->setLabel("中文asd");
+        response($qrCode->get(QrCode::IMAGE_TYPE_PNG))
+            ->cacheControl("no-store, no-cache, must-revalidate, post-check=0, pre-check=0")
+            ->contentType("image/png")
+            ->send();
     }
 }
