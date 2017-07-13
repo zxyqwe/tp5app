117c005c62a25ba507a0c13f2021ca74c5f301c5
张小奕 <zxyqwe_2004@126.com>
2017-07-13 10:47:18 +0800

    page

--- application/hanbj/controller/Index.php
+++ application/hanbj/controller/Index.php
@@ -9,6 +9,6 @@ class Index
     {
         $access = WX_access(config('hanbj_api'), config('hanbj_secret'), 'HANBJ_ACCESS');
         $access = substr($access, 0, 5);
-        return view('login', ['acc' => $access]);
+        return view('home', ['acc' => $access]);
     }
 }
