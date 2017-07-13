f434b4d7b16ca4312aa02fb05fda17497dd8038f
张小奕 <zxyqwe_2004@126.com>
2017-07-13 11:19:36 +0800

    capt

--- application/hanbj/controller/Index.php
+++ application/hanbj/controller/Index.php
@@ -2,6 +2,8 @@
 
 namespace app\hanbj\controller;
 
+use think\captcha;
+
 
 class Index
 {
@@ -9,6 +11,14 @@ class Index
     {
         $access = WX_access(config('hanbj_api'), config('hanbj_secret'), 'HANBJ_ACCESS');
         $access = substr($access, 0, 5);
-        return view('home', ['acc' => $access]);
+        return view('login', ['acc' => $access]);
+    }
+
+    public function json_login()
+    {
+        $capt = input('post.capt');
+        if (!captcha_check($capt)) {
+
+        }
     }
 }
