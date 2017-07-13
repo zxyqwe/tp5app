1d9bcf9470a9a28468da2a00dbf57c73b44aad41
张小奕 <zxyqwe_2004@126.com>
2017-07-13 12:32:30 +0800

    login

--- application/hanbj/controller/Index.php
+++ application/hanbj/controller/Index.php
@@ -9,16 +9,35 @@ class Index
 {
     public function index()
     {
-        $access = WX_access(config('hanbj_api'), config('hanbj_secret'), 'HANBJ_ACCESS');
-        $access = substr($access, 0, 5);
-        return view('login', ['acc' => $access]);
+        //$access = WX_access(config('hanbj_api'), config('hanbj_secret'), 'HANBJ_ACCESS');
+        //$access = substr($access, 0, 5);
+        if ('succ' === session('login')) {
+            redirect('/hanbj/index/home');
+        }
+        $nonstr = getNonceStr();
+        session('nonstr', $nonstr);
+        return view('login', ['nonstr' => $nonstr]);
     }
 
     public function json_login()
     {
         $capt = input('post.capt');
         if (!captcha_check($capt)) {
-
+            return json(['msg' => '验证码错误'], 400);
         }
+        $mm = input('post.mm');
+        $user = input('post.user');
+        $nonstr = session('nonstr');
+        $tmp = '';
+        if ($mm !== $tmp) {
+            return json(['msg' => '密码错误'], 400);
+        }
+        session('login', 'succ');
+        return json(['msg' => ' 登录成功'], 200);
+    }
+
+    public function home()
+    {
+        
     }
 }
