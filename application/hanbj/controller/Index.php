bccc81136e43ac9789b57250a97c4ea3488f1569
张小奕 <zxyqwe_2004@126.com>
2017-07-13 10:26:19 +0800

    html

--- application/hanbj/controller/Index.php
+++ application/hanbj/controller/Index.php
@@ -8,6 +8,7 @@ class Index
     public function index()
     {
         $access = WX_access(config('hanbj_api'), config('hanbj_secret'), 'HANBJ_ACCESS');
-        return substr($access, 0, 5);
+        $access = substr($access, 0, 5);
+        return view('login', ['acc' => $access]);
     }
 }
