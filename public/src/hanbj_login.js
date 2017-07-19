e3d2503ec03f4033ae2eec484c0645fb6937b3eb
张小奕 <zxyqwe_2004@126.com>
2017-07-19 21:57:58 +0800

    login js

--- public/src/hanbj_login.js
+++ public/src/hanbj_login.js
@@ -1,10 +1,36 @@
-var bulletin = (function ($, w, undefined) {
+var login = (function ($, w, undefined) {
     'use strict';
+    var $mm = $('#mm');
     var init = function () {
-
+        $('#capt_img').click(function () {
+            $('#capt_img').attr('src', '/captcha.html?' + new Date());
+        });
+        $("#login").click(function () {
+            var mm = $mm.val();
+            mm = CryptoJS.SHA1(mm).toString();
+            mm += w.nonStr;
+            mm = CryptoJS.SHA1(mm).toString();
+            $mm.val(mm);
+            var d = $('#form').serializeArray();
+            $.ajax({
+                type: "POST",
+                url: "/hanbj/data/json_login",
+                data: d,
+                dataType: "json",
+                success: function (msg) {
+                    location.reload(true);
+                },
+                error: function (msg) {
+                    msg = JSON.parse(msg.responseText);
+                    alert(msg.msg);
+                    $('#capt_img').attr('src', '/captcha.html?' + new Date());
+                    $('#mm').val('');
+                    $('#capt').val('');
+                }
+            });
+        });
     };
     return {
         init: init
     };
-
 })(jQuery, window);
