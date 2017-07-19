f29c18143a3132fd169fa3a64adfbbfad22d9217
张小奕 <zxyqwe_2004@126.com>
2017-07-19 21:54:03 +0800

    bulletin js

--- public/src/hanbj_bulletin.js
+++ public/src/hanbj_bulletin.js
@@ -1,10 +1,43 @@
 var bulletin = (function ($, w, undefined) {
     'use strict';
-    var init = function () {
+    var repeat = function (target, n) {
+        var s = target, total = "";
+        while (n > 0) {
+            if (n % 2 == 1) {
+                total += s;
+            }
+            if (n == 1) {
+                break;
+            }
 
+            s += s;
+            n = n >> 1;//相当于将n除以2取其商，或者说是开2次方
+        }
+        return total;
+    };
+    var init = function () {
+        $('#back').click(function () {
+            location.href = '/hanbj';
+        });
+        var $table = $('#table');
+        var alr = $('#ggly').html();
+        var nye = $('#rgly').html();
+        var time = new Date();
+        time = time.getFullYear() + 1;
+        $table.bootstrapTable({
+            'pageSize': 20,
+            responseHandler: function (res) {
+                var msg = res.rows;
+                for (var i in msg) {
+                    var tmp = time - parseInt(msg[i].t) - parseInt(msg[i].s);
+                    tmp = tmp > 0 ? tmp : 0;
+                    msg[i].o = repeat(alr, parseInt(msg[i].s)) + repeat(nye, tmp);
+                }
+                return res;
+            }
+        });
     };
     return {
         init: init
     };
-
 })(jQuery, window);
