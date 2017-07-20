3693ae2075757ceb4b8ba3340cc7d745583963de
张小奕 <zxyqwe_2004@126.com>
2017-07-20 11:06:39 +0800

    sha1

--- application/hanbj/controller/Mobile.php
+++ application/hanbj/controller/Mobile.php
@@ -2,7 +2,10 @@
 
 namespace app\hanbj\controller;
 
+include_once APP_PATH . 'wx.php';
 
+use app\SHA1;
+use app\WXBizMsgCrypt;
 use think\Db;
 
 class Mobile
@@ -31,7 +34,34 @@ class Mobile
 
     public function event()
     {
-        $access = WX_access(config('hanbj_api'), config('hanbj_secret'), 'HANBJ_ACCESS');
-        return substr($access, 0, 5);
+        //$access = WX_access(config('hanbj_api'), config('hanbj_secret'), 'HANBJ_ACCESS');
+        //return substr($access, 0, 5);
+        //simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+        $token = config('hanbj_token');
+        $aes = config('hanbj_EncodingAESKey');
+        $api = config('hanbj_api');
+        $post_data = $GLOBALS["HTTP_RAW_POST_DATA"];
+        $msg_sign = input('get.msg_signature');
+        $sign = input('get.signature');
+        $timestap = input('get.timestamp');
+        $nonce = input('get.nonce');
+        $pc = new SHA1();
+        $err = $pc->getSHA1($token, $timestap, $nonce, '');
+        if ($err[1] !== $sign) {
+            return 'fail';
+        }
+        $pc = new WXBizMsgCrypt($token, $aes, $api);
+        $msg = '';
+        $err = $pc->decryptMsg($msg_sign, $timestap, $nonce, $post_data, $msg);
+        if ($err !== 0) {
+            trace(['dec', $err]);
+        }
+        trace($msg);
+        $reply = '';
+        $err = $pc->encryptMsg($msg, time(), getNonceStr(), $reply);
+        if ($err !== 0) {
+            trace(['enc', $err]);
+        }
+        return $reply;
     }
 }
