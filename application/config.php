5afa2cb126d9b0174d8c40b9e7f95fc9d83af080
张小奕 <zxyqwe_2004@126.com>
2017-07-20 09:00:51 +0800

    sensitive

--- application/config.php
+++ application/config.php
@@ -21,7 +21,7 @@ return [
     // 应用Trace
     'app_trace' => false,
     // 应用模式状态
-    'app_status' => '',
+    'app_status' => 'sensitive',
     // 是否支持多模块
     'app_multi_module' => true,
     // 入口自动绑定模块
