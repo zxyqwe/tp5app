27be8219251c9223c987145a888c6634678ed3f8
张小奕 <zxyqwe_2004@126.com>
2017-07-23 10:12:43 +0800

    tomsg

--- public/src/hanbj_old.js
+++ public/src/hanbj_old.js
@@ -1,11 +1,22 @@
 var Old = (function ($, w, undefined) {
     'use strict';
+    var $toast = $('#toast');
+    var $old_msg = $('#old_msg');
+    var msgto = function (data) {
+        old_msg.html(data);
+        if ($toast.css('display') != 'none') return;
+
+        $toast.fadeIn(100);
+        setTimeout(function () {
+            $toast.fadeOut(100);
+        }, 2000);
+    };
     var init = function () {
         $("#oldok").click(function () {
             var weuiAgree = $('#weuiAgree').get(0).checked;
             if (!weuiAgree) {
+                msgto('阅读并同意《相关条款》');
                 return;
-                //ToDo
             }
             var old_eid = $('#old_eid').val();
             var old_phone = $('#old_phone').val();
@@ -22,8 +33,7 @@ var Old = (function ($, w, undefined) {
                 },
                 error: function (msg) {
                     msg = JSON.parse(msg.responseText);
-                    alert(msg.msg);
-                    //ToDo
+                    msgto(msg.msg);
                 }
             });
         });
