3d270996c737f8ddaff512eb33ce28039f4fe7d2
张小奕 <zxyqwe_2004@126.com>
2017-07-23 14:23:47 +0800

    home js

---
+++ public/src/hanbj_home.js
@@ -0,0 +1,37 @@
+var home = (function ($, w, undefined) {
+    'use strict';
+    var $toast, $old_msg;
+    var msgto = function (data) {
+        $old_msg.html(data);
+        var tdis = $toast.css('display');
+        if ('none' !== tdis)
+            return;
+
+        $toast.fadeIn(100);
+        setTimeout(function () {
+            $toast.fadeOut(100);
+        }, 2000);
+    };
+    var init = function () {
+        $toast = $('#toast');
+        $old_msg = $('#old_msg');
+        w.status.css({"display": "block"});
+        $("#card0").click(function () {
+            $.ajax({
+                type: "GET",
+                url: "/hanbj/mobile/json_card",
+                dataType: "json",
+                success: function (msg) {
+                    location.reload(true);
+                },
+                error: function (msg) {
+                    msg = JSON.parse(msg.responseText);
+                    msgto(msg.msg);
+                }
+            });
+        });
+    };
+    return {
+        init: init
+    };
+})(Zepto, window);
