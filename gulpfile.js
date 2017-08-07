176e0ffaeee9fee315a180718b5b6594bf7a72ec
张小奕 <zxyqwe_2004@126.com>
2017-08-07 15:19:59 +0800

    seq

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,30 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     cssnano = require('gulp-cssnano'),
     rev = require('gulp-rev'),
-    revCollector = require("gulp-rev-collector");
-gulp.task('install', function () {
+    revCollector = require("gulp-rev-collector"),
+    gulpSequence = require('gulp-sequence');
+
+gulp.task('js', function () {
     gulp.src('public/src/*.js')
         .pipe(rev())
         .pipe(uglify({mangle: true}))
         .pipe(gulp.dest('public/static/'))
         .pipe(rev.manifest())
         .pipe(gulp.dest('public/static/rev/js'));
+});
+gulp.task('css', function () {
     gulp.src('public/src/*.css')
         .pipe(rev())
         .pipe(cssnano())
         .pipe(gulp.dest('public/static/'))
         .pipe(rev.manifest())
         .pipe(gulp.dest('public/static/rev/css'));
-    gulp.src(['public/static/rev/**/*.json', 'application/hanbj/tpl/*.html'])
-        .pipe( revCollector({
-            replaceReved: true
-        }) )
-        .pipe( gulp.dest('application/hanbj/view/') );
 });
+gulp.task('hanbj_tpl', function () {
+    gulp.src(['public/static/rev/**/*.json', 'application/hanbj/tpl/*.html'])
+        .pipe(revCollector({
+            replaceReved: true
+        }))
+        .pipe(gulp.dest('application/hanbj/view/'));
+});
+gulp.task('hanbj', gulpSequence(['js', 'css'], 'hanbj_tpl'));
