faa86f204a65427e6b3f721f65e2caf27bbb9638
张小奕 <zxyqwe_2004@126.com>
2017-08-28 10:43:58 +0800

    prefix

--- application/index/controller/Bilibili.php
+++ application/index/controller/Bilibili.php
@@ -11,8 +11,8 @@ class Bilibili
 
     public function index()
     {
-        $past = cache('cron_user_past');
-        $cur = cache('cron_user');
+        $past = cache('bili_cron_user_past');
+        $cur = cache('bili_cron_user');
         $past = json_decode($past, true);
         $cur = json_decode($cur, true);
         $past_intimacy = 0;
@@ -34,21 +34,22 @@ class Bilibili
     public function cron()
     {
         $time = date("Y-m-d H:i:s");
-        if (cache('?cron_cookie')) {
+        if (cache('?bili_cron_cookie')) {
             return json(['msg' => 'too fast', 'time' => $time]);
         }
-        cache('cron_cookie', 'cron_cookie', 290);
-        $cookie = config('cron_cookie');
+        cache('bili_cron_cookie', 'bili_cron_cookie', 290);
+        $cookie = config('bili_cron_cookie');
         $urlapi = 'https://api.live.bilibili.com/User/userOnlineHeart';
         $res = bili_Post($urlapi, $cookie, 218);
         $res = json_decode($res, true);
         if ($res['code'] !== 0) {
             trace(json_encode($res));
         }
-        cache('cron_user_past', cache('cron_user'));
+        cache('bili_cron_user_past', cache('bili_cron_user'));
         $urlapi = 'http://live.bilibili.com/User/getUserInfo';
         $res = bili_Post($urlapi, $cookie, 218);
-        cache('cron_user', $res);
+        cache('bili_cron_user', $res);
+        cache('bili_cron_time', $time);
         return json(['msg' => 'ok', 'time' => $time]);
     }
 }
