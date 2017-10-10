b045dbc2e8cb646afddeca24c2943011658d27e0
张小奕 <zxyqwe_2004@126.com>
2017-10-09 21:01:54 -0400

    struct

--- public/src/wx_test.js
+++ public/src/wx_test.js
@@ -1,7 +1,28 @@
 var wx_test = (function ($, Vue, w, undefined) {
     'use strict';
-    var test2017 = {};
+    var vmain, vtest, obj;
+    var test2017 = {uname: ['素问', '采峦'], test: []};
     var init = function () {
+        var answer = test2017;
+        vmain = new Vue({
+            el: '#objmain',
+            data: {
+                uname: answer.uname
+            },
+            methods: {
+                sel_name: function (item) {
+                    obj = item;
+                }
+            }
+        });
+        vtest = new Vue({
+            el: '#testmain',
+            data: {
+                uname: obj,
+                test: answer.test
+            },
+            methods: {}
+        });
     };
     return {
         init: init
