9337ddff178890272209ddfbc51d9ccf4c109991
张小奕 <zxyqwe_2004@126.com>
2017-11-14 17:20:47 -0500

    nav file

--- public/src/hanbj_base.js
+++ public/src/hanbj_base.js
@@ -43,3 +43,38 @@ var home = (function ($, w, undefined) {
         mem_code: mem_code
     };
 })(jQuery, window);
+
+var nav_active = (function ($, w, undefined) {
+    'use strict';
+    var dict = function () {
+        var base = '/hanbj/';
+        var data = 'data/';
+        var dataopen = 'dataopen/';
+        var index = 'index/';
+        w.u1 = base + data + 'fee_search';
+        w.u2 = base + data + 'fee_add';
+        w.u3 = base + index + 'feelog';
+        w.u4 = base + data + 'vol_add';
+        w.u5 = base + index + 'actlog';
+        w.u6 = base + data + 'bonus_add';
+        w.u7 = base + data + 'json_detail';
+        w.u8 = base + data + 'json_tree';
+        w.u9 = base + dataopen + 'json_fame';
+        w.u10 = base + data + 'fame_add';
+        w.u11 = base + dataopen + 'json_login';
+        w.u12 = base + index + 'runlog/data/1';
+    };
+    var init = function () {
+        dict();
+        var nav = w.nav;
+        if (nav === undefined)
+            return;
+        while (nav.length > 0) {
+            $('#' + nav).addClass('active');
+            nav = nav.substring(0, nav.lastIndexOf('_'));
+        }
+    };
+    return {
+        init: init
+    };
+})(jQuery, window);
