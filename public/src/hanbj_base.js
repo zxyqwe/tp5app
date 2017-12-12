f578d664c9371af59785b4e02e32cd401d9373f3
张小奕 <zxyqwe_2004@126.com>
2017-12-12 14:19:33 -0500

    grade

--- public/src/hanbj_base.js
+++ public/src/hanbj_base.js
@@ -62,9 +62,39 @@ var home = (function ($, w, undefined) {
                 return value;
         }
     };
+    var grade = function (n) {
+        switch (n) {
+            case '0':
+                return '会长';
+            case '1':
+                return '副会长';
+            case '2':
+                return '部长';
+            case '3':
+                return '副部长';
+            case '4':
+                return '干事';
+        }
+    };
+    var fame_img= function (n) {
+        switch (n) {
+            case '0':
+                return '/static/arrow-up.png';
+            case '1':
+                return '/static/arrow-up.png';
+            case '2':
+                return '/static/arrow-up.png';
+            case '3':
+                return '/static/arrow-up.png';
+            case '4':
+                return '/static/arrow-up.png';
+        }
+    };
     return {
         init: init,
-        mem_code: mem_code
+        mem_code: mem_code,
+        grade: grade,
+        fame_img:fame_img
     };
 })(jQuery, window);
 
