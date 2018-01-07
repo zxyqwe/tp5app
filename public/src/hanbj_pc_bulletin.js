7b31e46d5cdf778cbf3d9a7954d354a3fc37e379
张小奕 <zxyqwe_2004@126.com>
2018-01-07 10:19:41 -0500

    js

--- public/src/hanbj_pc_bulletin.js
+++ public/src/hanbj_pc_bulletin.js
@@ -1,20 +1,5 @@
 var bulletin = (function ($, w, undefined) {
     'use strict';
-    var repeat = function (target, n) {
-        var s = target, total = "";
-        while (n > 0) {
-            if (n % 2 === 1) {
-                total += s;
-            }
-            if (n === 1) {
-                break;
-            }
-
-            s += s;
-            n = n >> 1;//相当于将n除以2取其商，或者说是开2次方
-        }
-        return total;
-    };
     var init = function () {
         var $table = $('#table');
         var alr = $('#ggly').html();
@@ -27,8 +12,7 @@ var bulletin = (function ($, w, undefined) {
                 var msg = res.rows;
                 for (var i in msg) {
                     var tmp = time - parseInt(msg[i].t) - msg[i].b;
-                    tmp = tmp > 0 ? tmp : 0;
-                    msg[i].o = repeat(alr, msg[i].b) + repeat(nye, tmp);
+                    msg[i].o = w.repeat_icon(alr, msg[i].b) + w.repeat_icon(nye, tmp);
                 }
                 return res;
             },
