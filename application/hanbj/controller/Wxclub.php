d2ce3cd8635d65687fb226822b9c0c7081db212b
张小奕 <zxyqwe_2004@126.com>
2018-06-11 15:56:53 -0400

    new wxclub

---
+++ application/hanbj/controller/Wxclub.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\hanbj\controller;
+
+use hanbj\BonusOper;
+use hanbj\MemberOper;
+use hanbj\OrderOper;
+use hanbj\weixin\HanbjRes;
+use hanbj\vote\WxOrg;
+use think\Controller;
+use think\Db;
+use hanbj\weixin\HanbjNotify;
+use hanbj\FeeOper;
+use app\hanbj\WxPayConfig;
+use app\WxPayUnifiedOrder;
+use app\WxPayApi;
+use think\exception\HttpResponseException;
+
+class Wxclub extends Controller
+{
+    protected $beforeActionList = [
+        'valid_id'
+    ];
+
+    protected function valid_id()
+    {
+        if (!MemberOper::wx_login()) {
+            $res = json(['msg' => '未登录'], 400);
+            throw new HttpResponseException($res);
+        }
+    }
+
+    public function _empty()
+    {
+        return json([], 404);
+    }
+
+    public function index()
+    {
+        return view('home');
+    }
+}
