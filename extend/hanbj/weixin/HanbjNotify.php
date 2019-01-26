a64e817dd9370863e5af00e80c3b56df1daf1bc5
张小奕 <zxyqwe_2004@126.com>
2019-01-26 23:21:51 +0800

    bugfix

--- extend/hanbj/weixin/HanbjNotify.php
+++ extend/hanbj/weixin/HanbjNotify.php
@@ -6,6 +6,7 @@ use wxsdk\pay\WxPayOrderQuery;
 use wxsdk\pay\WxPayApi;
 use wxsdk\pay\WxPayNotify;
 use hanbj\OrderOper;
+use Exception;
 
 class HanbjNotify extends WxPayNotify
 {
@@ -19,20 +20,42 @@ class HanbjNotify extends WxPayNotify
             && $result["return_code"] == "SUCCESS"
             && $result["result_code"] == "SUCCESS"
         ) {
-            return true;
+            if (array_key_exists("trade_state", $result)
+                && $result['trade_state'] == "SUCCESS"
+            ) {
+                return true;
+            } else {
+                trace("orderQuery " . json_encode($result));
+            }
         }
         return false;
     }
 
-    public function NotifyProcess($data, $config, &$msg)
+    public function NotifyProcess($objData, $config, &$msg)
     {
         $msg = 'OK';
-        $data = $data->GetValues();
+        $data = $objData->GetValues();
+        if (!array_key_exists("return_code", $data)
+            || (array_key_exists("return_code", $data) && $data['return_code'] != "SUCCESS")
+        ) {
+            return false;
+        }
         if (!array_key_exists("out_trade_no", $data)) {
             return false;
         }
+        try {
+            $checkResult = $objData->CheckSign($config);
+            if ($checkResult == false) {
+                return false;
+            }
+        } catch (Exception $e) {
+            trace('检查签名 ' . json_encode($e));
+        }
         //查询订单，判断订单真实性
         if (!$this->Queryorder($data["out_trade_no"])) {
+            return false;
+        }
+        if (!array_key_exists("transaction_id", $data)) {
             return false;
         }
         return OrderOper::handle($data);
