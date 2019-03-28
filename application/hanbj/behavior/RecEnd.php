eadfde068e880b4e1ea814698e3d7ebe8a0af34d
张小奕 <zxyqwe_2004@126.com>
2019-03-28 21:55:27 +0800

    modify tags

---
+++ application/hanbj/behavior/RecEnd.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\hanbj\behavior;
+
+use \think\Debug;
+use \think\App;
+
+class RecEnd
+{
+    /*
+     * @param \think\Response
+     */
+    public function run(&$response)
+    {
+        if (defined('TAG_TIMEOUT_EXCEPTION')) {
+            WX_access(config('hanbj_api'), config('hanbj_secret'), 'HANBJ_ACCESS');
+            return;
+        }
+        $runtime = Debug::getUseTime();
+        if ($runtime > 1.5) {
+            App::$debug = true;
+            Debug::remark('behavior_start', $runtime);
+            $ret = [
+                'GET' => $_GET,
+                'POST' => $_POST,
+                'Files' => $_FILES,
+                'Cookies' => $_COOKIE,
+                'Session' => isset($_SESSION) ? $_SESSION : [],
+                'Server' => $_SERVER,
+                'Env' => $_ENV
+            ];
+            $ret = explode_dict($ret);
+            foreach ($ret as $i) {
+                trace($i, 'debug');
+            }
+        }
+    }
+}
