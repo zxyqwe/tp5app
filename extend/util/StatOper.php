701b126c8e969cf4154f174f12893501dd4e9609
张小奕 <zxyqwe_2004@126.com>
2019-05-27 23:28:59 +0800

    add stat

---
+++ extend/util/StatOper.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace util;
+
+
+class StatOper
+{
+    const LOG_NUM = 0;
+}
+
+/*
+CREATE TABLE `stat` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(11) NOT NULL,
+  `content` varchar(2048) NOT NULL,
+`time` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+*/
