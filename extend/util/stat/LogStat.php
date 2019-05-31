181f8a7e44295296e57d18d2de0a96f2f21eb9fe
张小奕 <zxyqwe_2004@126.com>
2019-05-31 23:22:33 +0800

    try new fig

--- extend/util/stat/LogStat.php
+++ extend/util/stat/LogStat.php
@@ -54,6 +54,40 @@ class LogStat extends BaseStat
 
     public function OutputAll()
     {
-        // TODO: Implement OutputAll() method.
+        $all_catg = MysqlLog::get_level();
+        $template = [];
+        foreach ($all_catg as $item) {
+            $template[$item] = [];
+        }
+        $time_range = [];
+        $content = StatOper::getQuery(StatOper::LOG_NUM)
+            ->order('time asc')
+            ->field(['time', 'content'])
+            ->cache(3600)
+            ->select();
+        foreach ($content as $item) {
+            $time_range[] = $item['time'];
+            $data = json_decode($item['content'], true);
+            $data = $this->build_kv($data);
+            foreach ($all_catg as $k) {
+                $template[$k][] = $data[$k];
+            }
+        }
+        return [$time_range, $template];
+    }
+
+    private function build_kv($select_ret)
+    {
+        $all_catg = MysqlLog::get_level();
+        $data = [];
+        foreach ($select_ret as $item) {
+            $data[$item['type']] = $item['num'];
+        }
+        foreach ($all_catg as $item) {
+            if (!isset($data[$item])) {
+                $data[$item] = 0;
+            }
+        }
+        return $data;
     }
 }
